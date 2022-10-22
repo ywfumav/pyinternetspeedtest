046c79af331f04807f8b40cf17e81f570b6454cd
ywfumav <101041538+ywfumav@users.noreply.github.com>
2022-10-22 16:38:50 +0800

    Internet speed test to perform download, upload and ping test.

---
+++ main.py
@@ -0,0 +1,22 @@
+import speedtest
+
+test = speedtest.Speedtest(secure=True)
+
+print("Loading server list...")
+test.get_servers()
+print("Choosing best servers...")
+best = test.get_best_server()
+print("Best Server is:")
+print("Hostname: {}\nLocation: {} \n".format(best['host'], best['country']))
+
+print("Performing Download Test...")
+download_result = test.download()
+print("Download Speed: {:.2f} Mbps".format(download_result/1024/1024))
+
+print("Performing Upload Test...")
+upload_result = test.upload()
+print("Upload Speed: {:.2f} Mbps".format(upload_result/1024/1024))
+
+print("Performing Ping Test...")
+ping_result = test.results.ping
+print("Ping Latency: {:.2f}".format(ping_result))
