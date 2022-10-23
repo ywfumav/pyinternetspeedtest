49ef0e4ee419f1cb08d8df545d6f504dc024a583
ywfumav <101041538+ywfumav@users.noreply.github.com>
2022-10-23 11:05:28 +0800

    updated main.py

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # pyinternetspeedtest
 An internet speed test monitor.
+
+**pyinternetspeedtest** will conduct a internet speed test using **speedtest-cli**. Then it will save test values into a **sql database**. User can monitor internet performance from a local webpage hosted on **node red**.
+
+## Hardware Information
+Hardware: **Raspberry Pi 4 Model B** <br>
+OS: **Raspbian Buster Lite**
+
+## Code Flowchart
+```mermaid
+graph TD;
+A[<font size = 3>main.py]-->B[<font size = 3>speedtest-cli<br><font size = 2>conduct internet speed test every minute] 
+B-->A
+A-->C[<font size = 3>database<br><font size = 2>store speed test results<br>datetime<br>download speed<br>upload speed<br>ping]
+C-->D[<font size = 3>Web GUI<br><font size = 2>Local Web interface to monitor <br> speed test results ]
+```
+
+## Installation
--- main.py
+++ main.py
@@ -1,7 +1,17 @@
 import time
 import speedtest
+import mysql.connector
 
-epoch_time = int(time.time())
+mydb = mysql.connector.connect(
+    host = 'localhost',
+    user = 'pi',
+    password = 'pi',
+    database = 'wan',
+)
+
+mycursor = mydb.cursor()
+
+epoch_time = int(time.time()*1000)
 print("Epoch time is {}".format(epoch_time))
 
 test = speedtest.Speedtest(secure=True)
@@ -13,13 +23,23 @@ print("Best Server is:")
 print("Hostname: {}\nLocation: {} \n".format(best['host'], best['country']))
 
 print("Performing Download Test...")
-download_result = test.download()
-print("Download Speed: {:.2f} Mbps".format(download_result/1024/1024))
+download_result = test.download()/1024/1024
+print("Download Speed: {:.2f} Mbps".format(download_result))
 
 print("Performing Upload Test...")
-upload_result = test.upload()
-print("Upload Speed: {:.2f} Mbps".format(upload_result/1024/1024))
+upload_result = test.upload()/1024/1024
+print("Upload Speed: {:.2f} Mbps".format(upload_result))
 
 print("Performing Ping Test...")
 ping_result = test.results.ping
-print("Ping Latency: {:.2f} ms".format(ping_result))
+print("Ping Latency: {:.2f} ms \n ".format(ping_result))
+
+sql = ("INSERT INTO internet (epoch, download, upload, ping) VALUES ({},{},{},{})".format(epoch_time, download_result, upload_result, ping_result))
+
+
+mycursor.execute(sql)
+mydb.commit()
+
+print(mycursor.rowcount, "details inserted")
+
+mydb.close()
